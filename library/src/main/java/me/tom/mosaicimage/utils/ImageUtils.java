c756b68648cf40ee5ab9619e7469992733a87e2d
Tom.Huang <hzlhu.dargon@gmail.com>
2017-04-30 14:51:19 +0800

    add ImageUtils to transform bitmap to mosaic bitmap

---
+++ library/src/main/java/me/tom/mosaicimage/utils/ImageUtils.java
@@ -0,0 +1,23 @@
+package me.tom.mosaicimage.utils;
+
+import android.graphics.Bitmap;
+
+public class ImageUtils {
+
+    public static Bitmap mosaic(Bitmap source) {
+        return mosaic(source, 16);
+    }
+
+    public static Bitmap mosaic(Bitmap source, int size) {
+        int sourceWidth = source.getWidth();
+        int sourceHeight = source.getHeight();
+        Bitmap destBitmap = source.copy(Bitmap.Config.ARGB_8888, true);
+        for (int x = 0; x < sourceWidth; x++) {
+            for (int y = 0; y < sourceHeight; y++) {
+                int pixel = source.getPixel(x / size * size, y / size * size);
+                destBitmap.setPixel(x, y, pixel);
+            }
+        }
+        return destBitmap;
+    }
+}
