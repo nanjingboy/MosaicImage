c51afb52d2226fab832ceaab06a805440185ac80
Tom.Huang <hzlhu.dargon@gmail.com>
2017-05-02 19:20:04 +0800

    change args name for ImageUtils#mosaic

--- library/src/main/java/me/tom/mosaicimage/utils/ImageUtils.java
+++ library/src/main/java/me/tom/mosaicimage/utils/ImageUtils.java
@@ -20,13 +20,13 @@ import rx.Observable;
 
 public class ImageUtils {
 
-    public static Bitmap mosaic(Bitmap source, int size) {
+    public static Bitmap mosaic(Bitmap source, int mosaicAreaSize) {
         int sourceWidth = source.getWidth();
         int sourceHeight = source.getHeight();
         Bitmap destBitmap = source.copy(Bitmap.Config.ARGB_8888, true);
         for (int x = 0; x < sourceWidth; x++) {
             for (int y = 0; y < sourceHeight; y++) {
-                int pixel = source.getPixel(x / size * size, y / size * size);
+                int pixel = source.getPixel(x / mosaicAreaSize * mosaicAreaSize, y / mosaicAreaSize * mosaicAreaSize);
                 destBitmap.setPixel(x, y, pixel);
             }
         }
